477a3702b8ff743080fb88bd8454640c7ddef9f4
sinusoidus <joeodams@gmail.com>
2017-11-22 17:51:49 +0000

    Add files via upload

--- html/query.php
+++ html/query.php
@@ -10,7 +10,7 @@ $long = $coord["loc"][1];
 
 $servername = "localhost";
 $username = "root";
-//$password = "3in9h6u3kj5zvkTL";
+$password = " ";
 $dbname = "geodem";
 
 // Create connection
@@ -20,24 +20,53 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$db = "USE geodem;";
+
+$conn->query($db);
+
 switch ($type) {
   case 0:
-    $sql = "SELECT pcd.Postcode, Latitude, Longitude, Total
+    $init = "CREATE TABLE temptable AS (SELECT pcd.Postcode, Latitude, Longitude, Total
     FROM geodem.uk_postcodes AS pcd
     INNER JOIN geodem.populations AS pop ON pop.pcd_fix = pcd.pcd_fix
-    WHERE Latitude BETWEEN ($lat - 0.2) AND ($lat + 0.2) AND Longitude BETWEEN ($long - 0.2) AND ($long + 0.2)";
+    WHERE Latitude BETWEEN ($lat - 0.2) AND ($lat + 0.2) AND Longitude BETWEEN ($long - 0.2) AND ($long + 0.2))";
+
+    $sql = "SELECT * FROM temptable";
+
+    $maxsql = "SELECT AVG(Total) AS Average, STDDEV(Total) AS Stddev,MAX(Total) AS Maximum
+    FROM temptable";
     break;
 
   case 1:
-    $sql = "SELECT pcd.Postcode, Latitude, Longitude, (Total / 5000) AS Total
+    $init = "CREATE TABLE temptable AS (SELECT pcd.Postcode, Latitude, Longitude, (Total / 5000) AS Total
     FROM geodem.uk_postcodes AS pcd
     INNER JOIN geodem.house_prices AS ppd ON ppd.pcd_fix = pcd.pcd_fix
-    WHERE Latitude BETWEEN ($lat - 0.2) AND ($lat + 0.2) AND Longitude BETWEEN ($long - 0.2) AND ($long + 0.2)";
+    WHERE Latitude BETWEEN ($lat - 0.2) AND ($lat + 0.2) AND Longitude BETWEEN ($long - 0.2) AND ($long + 0.2))";
+
+    $sql = "SELECT * FROM temptable";
+
+    $maxsql = "SELECT AVG(Total) AS Average, STDDEV(Total) AS Stddev,MAX(Total) AS Maximum
+    FROM temptable";
     break;
 }
 
+$drop = "DROP TABLE temptable";
 
+$conn->query($drop);
 
+if (!$result = $conn->query($init)) {
+    // Oh no! The query failed.
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo $conn->host_info . "\n";
+    echo $username . "\n";
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $conn->errno . "\n";
+    echo "Error: " . $conn->error . "\n";
+    exit;
+}
 
 if (!$result = $conn->query($sql)) {
     // Oh no! The query failed.
@@ -53,10 +82,31 @@ if (!$result = $conn->query($sql)) {
     exit;
 }
 
+if (!$maxresult = $conn->query($maxsql)) {
+    // Oh no! The query failed.
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo $conn->host_info . "\n";
+    echo $username . "\n";
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $conn->errno . "\n";
+    echo "Error: " . $conn->error . "\n";
+    exit;
+}
+
+
 $rows = array();
+
+$maxrow = array();
 
 while($r = mysqli_fetch_assoc($result)) {
     $rows[] = $r;
+}
+
+while($m = mysqli_fetch_assoc($maxresult)) {
+    $maxrow[] = $m;
 }
 
 //echo $sql . "\n";
@@ -65,7 +115,8 @@ while($r = mysqli_fetch_assoc($result)) {
 
 //echo $type;
 //echo $long;
-echo json_encode($rows);
+
+echo json_encode(array($maxrow,$rows));
 
 
 
